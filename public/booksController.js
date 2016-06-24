d61070c9c61c2fd99651f06a9ab9de9d792d8c62
Prince <prince.vabraham@gmail.com>
2016-06-24 17:03:18 -0400

    editing edit buttons
    editing edit buttons

--- public/booksController.js
+++ public/booksController.js
@@ -1,15 +1,13 @@
 angular.module('bookstore',[])
     .controller('booksController', function($scope){    
-        console.log('control loaded');
-    $scope.isFormVisible= false;
-    
+   
     var products =
     [{        
         bookName:'The Giver',
         author: 'Lois Lowry',
         ISBN: 111,
         numBooks: 5,
-        pubDate: 9/2/16,
+        pubDate: '9/2/16',
         bookCat: 'Fiction',
         numBooksIssued: 4                    
     },        
@@ -18,48 +16,79 @@ angular.module('bookstore',[])
         author: 'John Doe',
         ISBN: '112',
         numBooks: 10,
-        pubDate: 8/12/96,
+        pubDate: '8/12/96',
         bookCat: 'biography',
-        numBooksIssued: 5
+        numBooksIssued: 0
         
     }];
     
-      $scope.addBook = function(bookData){
+     $scope.items = products; 
+    
+    //add button
+
+     $scope.add= function(){
+        $scope.isFormVisible= true; 
+        $scope.isEditVisible = false;
+    };
+     
+    //add submit button 
+      $scope.addSubmit = function(bookData){
           
           $scope.items.push(bookData);
           $scope.newData = {};
-          console.log(bookData);
-          
+          $scope.isFormVisible= false; 
+          //console.log(bookData);
+         
     };
-        
-       
-        $scope.items = products; 
-        //console.log($scope.items);
-        //console.log($scope.newData);
-    })
-
-   
-
-    .controller('tableController',function(){
-        this.row =1;
-        
-        this.setRow = function(newValue){
-            this.row = newValue;
-        }
-        
-        this.isSet = function(rowName){
-           return this.row === rowName;
-        }
-    });
-
-//forms display after buttons are clicked
-function myButton(){
-    var iframe = $("#showButton");
-    iframe.attr("src", iframe.data("src"));
     
-}
+    //getting selected row index
+        $scope.rowIndex= function(index){
+            $scope.selectIndex = index; 
+            //console.log($scope.selectIndex);
+        };
 
-//adjust iframe according to form contents
-$('iframe').load(function(){
-    $(this).height($(this).contents().outerHeight());
-});
+    // edit button
+        $scope.edit= function(){
+            var selectedIndex = $scope.selectIndex;
+            
+            if(selectedIndex == null || selectedIndex == undefined){
+                alert('Please Select a Row!');
+                return;
+            } 
+            
+            var obj = $scope.items[selectedIndex];
+            //console.log(obj);
+             
+            $scope.isEditVisible = true;
+            $scope.isFormVisible= false;
+            
+            $scope.current = obj;
+            
+        };
+    
+    //edit submit button
+    $scope.editSubmit = function(userInput){
+        
+       $scope.current.numBooks = userInput;
+       $scope.current = null;
+       $scope.selectIndex = null;
+       $scope.isEditVisible = false;
+    };
+    
+    //delete button
+    $scope.delete= function(){
+        var it = $scope.items;
+        
+        if(it[$scope.selectIndex].numBooksIssued == 0){
+            
+           it.splice($scope.selectIndex,1);  
+            return;
+        };
+        
+         $scope.isEditVisible = false;
+         $scope.isFormVisible= false;
+    
+    };
+       
+        
+    });
